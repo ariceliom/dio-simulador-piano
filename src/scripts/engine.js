a7cb7a7887dfa8f1096a4885a3a93cd2e5a8b621
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-05 17:42:22 -0300

    aula: Adicionando Controle de volumes

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,5 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let mappedKeys = [];
 let audio = new Audio("./src/tunes/a.wav");
@@ -24,3 +25,9 @@ document.addEventListener("keydown", (e) => {
     playTune(e.key);
   }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+}
+
+volumeSlider.addEventListener("input", handleVolume);
