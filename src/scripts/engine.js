575a515a6ac43fc5bdf9d75570d21fc7e60d20ce
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-05 17:37:18 -0300

    aula: Mapeando teclas permitidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mappedKeys = [];
 let audio = new Audio("./src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -14,10 +15,12 @@ const playTune = (key) => {
 }
 
 pianoKeys.forEach(key => {
-  console.log(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
+  mappedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-  playTune(e.key);
+  if(mappedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
